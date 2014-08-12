d0d0e7ec9811607ecc74f58c0cbfb3d2b49ac2ab
LiviaMiura <livataliba@gmail.com>
2014-08-12 09:02:25 -0300

    atualizado

---
+++ src/main/java/br/leona/controller/IndexController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.leona.controller;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+
+
+/**
+ *
+ * @author Jean Negrão
+ */
+@Resource
+public class IndexController {
+    private Result result;
+    public IndexController(Result result){
+        this.result = result;        
+    }    
+    @Path("/")
+    @SuppressWarnings("empty-statement")
+    public void index(){   
+       java.util.List<br.leona.station.controller.Servico> servicos = GetListService();
+        result                 
+                .include("listService", GetListService())
+                .forwardTo("index.jsp");
+    }    
+    
+    private static java.util.List<br.leona.station.controller.Servico> GetListService() {
+        br.leona.station.controller.ServicosController_Service service = new br.leona.station.controller.ServicosController_Service();
+        br.leona.station.controller.ServicosController port = service.getServicosControllerPort();
+        return port.getListService();
+    }
+
+}
